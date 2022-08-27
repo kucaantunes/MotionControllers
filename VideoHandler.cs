67d2af99d537cf334dec10f55c538150717240fa
Carlos Filipe Paiva da Cunha Macieira Antunes <kucaantunes@hotmail.com>
2022-08-28 00:30:33 +0100

    Add files via upload

---
+++ VideoHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class VideoHandler : MonoBehaviour, IPointerClickHandler {
+
+	public uint Uid
+	{
+		get { return uid; }
+		set { uid = value; }
+	}
+
+	private uint uid;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		InteractionHandler.Instance.SwapBackgroundVideo(uid);
+	}
+}
